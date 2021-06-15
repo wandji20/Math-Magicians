277e7b177045066111491ac99a88d90f4bdda474
wandji20 <wbertrand10@gmail.com>
2021-06-15 15:50:05 +0100

    Add Test component to index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+
+function Test() {
+  return (
+    <div>
+      Yooooo
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('root'),
+);
