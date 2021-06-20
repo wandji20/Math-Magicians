dbcb24f946c0c63242ecb6ec07d882f2e319cc93
wandji20 <wbertrand10@gmail.com>
2021-06-20 09:23:06 +0100

    Refactor class component to function component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Button from './Button';
 
-export default class ButtonPanel extends React.Component {
-  renderInput = (buttonName) => {
-    const { clickHandler } = this.props;
+const ButtonPanel = (props) => {
+  const renderInput = (buttonName) => {
+    const { clickHandler } = props;
     const operators = ['-', '+', '÷', 'x'];
+
     if (operators.includes(buttonName) || buttonName === '=') {
       return (
         <Button
@@ -29,44 +30,41 @@ export default class ButtonPanel extends React.Component {
         buttonName={buttonName}
       />
     );
-  }
-
-  render() {
-    return (
-      <div className="button-panel">
-        <div className="row">
-          {this.renderInput('AC')}
-          {this.renderInput('+/-')}
-          {this.renderInput('%')}
-          {this.renderInput('÷')}
-        </div>
-        <div className="row">
-          {this.renderInput('7')}
-          {this.renderInput('8')}
-          {this.renderInput('9')}
-          {this.renderInput('x')}
-        </div>
-        <div className="row">
-          {this.renderInput('4')}
-          {this.renderInput('5')}
-          {this.renderInput('6')}
-          {this.renderInput('-')}
-        </div>
-        <div className="row">
-          {this.renderInput('1')}
-          {this.renderInput('2')}
-          {this.renderInput('3')}
-          {this.renderInput('+')}
-        </div>
-        <div className="row">
-          {this.renderInput('0')}
-          {this.renderInput('.')}
-          {this.renderInput('=')}
-        </div>
+  };
+  return (
+    <div className="button-panel">
+      <div className="row">
+        { renderInput('AC')}
+        { renderInput('+/-')}
+        { renderInput('%')}
+        { renderInput('÷')}
       </div>
-    );
-  }
-}
+      <div className="row">
+        { renderInput('7')}
+        { renderInput('8')}
+        { renderInput('9')}
+        { renderInput('x')}
+      </div>
+      <div className="row">
+        { renderInput('4')}
+        { renderInput('5')}
+        { renderInput('6')}
+        { renderInput('-')}
+      </div>
+      <div className="row">
+        { renderInput('1')}
+        { renderInput('2')}
+        { renderInput('3')}
+        { renderInput('+')}
+      </div>
+      <div className="row">
+        { renderInput('0')}
+        { renderInput('.')}
+        { renderInput('=')}
+      </div>
+    </div>
+  );
+};
 
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func,
@@ -74,3 +72,5 @@ ButtonPanel.propTypes = {
 ButtonPanel.defaultProps = {
   clickHandler: () => {},
 };
+
+export default ButtonPanel;
