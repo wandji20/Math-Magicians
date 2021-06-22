c005cc731fd9d1bc06525c983d0cc30e1eb6fe68
wandji20 <wbertrand10@gmail.com>
2021-06-22 15:22:50 +0100

    Add snapshot test for Calculator component

---
+++ src/test/Calculator.test.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator'
+
+
+it('Component matches snapshot', () => {
+  const tree = renderer
+    .create(<Calculator/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
